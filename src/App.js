d07963d3d28d159372e0607aafcdd72b926e1df7
Kim Jae Doo <sprtms0101@naver.com>
2019-10-30 20:34:16 +0900

    8 commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { HashRouter as Router, Route } from "react-router-dom";
+import { ApolloProvider } from "react-apollo";
+import { ApolloProvider as ApolloHooksProvider } from "react-apollo-hooks";
+import client from "./apolloClient";
+import Home from "./Home";
+import Detail from "./Detail";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        
-      </div>
+      <ApolloProvider client={client}>
+        <ApolloHooksProvider client={client}>
+          <Router>
+            <main>
+              <Route exact={true} path={"/"} component={Home} />
+              <Route path={"/details/:movieId"} component={Detail} />
+            </main>
+          </Router>
+        </ApolloHooksProvider>
+      </ApolloProvider>
     );
   }
 }
